2d821954e4eca09c7129ed261a65527cdc12e8b4
indrapatel8 <ivfire811@gmail.com>
2023-02-07 11:53:02 +0530

    MainKT file Toast added

--- src/main/java/com/indra/calculatorapp/MainActivity.kt
+++ src/main/java/com/indra/calculatorapp/MainActivity.kt
@@ -2,10 +2,12 @@ package com.indra.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this,"OK",Toast.LENGTH_LONG).show()
     }
 }
