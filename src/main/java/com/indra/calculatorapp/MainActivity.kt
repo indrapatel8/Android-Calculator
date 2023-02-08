a656105321dfe2006e36ce56113eb101add76327
indrapatel8 <ivfire811@gmail.com>
2023-02-08 14:17:51 +0530

    ok

--- src/main/java/com/indra/calculatorapp/MainActivity.kt
+++ src/main/java/com/indra/calculatorapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.indra.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
